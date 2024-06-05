4bb87b44168cb7fc722fcd2a61a809f80c02c94c
cloudskies <cloudcloudskies@gmail.com>
2024-06-05 19:56:37 +0300

    Format the mod

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,60 @@
 #include <Geode/Geode.hpp>
-using namespace geode::prelude;
-
 #include <Geode/loader/SettingEvent.hpp>
 #include <Geode/modify/LoadingLayer.hpp>
+#include <Geode/modify/AppDelegate.hpp>
+#include <windows.h>
+
+using namespace geode::prelude;
+
 class $modify(LoadingLayer) {
     bool init(bool p0) {
         if (!LoadingLayer::init(p0)) return false;
-        auto GameWindowHandle = WindowFromDC(*reinterpret_cast<HDC*>(reinterpret_cast<uintptr_t>(cocos2d::CCEGLView::sharedOpenGLView()->getWindow()) + 0x244));
-        SetWindowText(GameWindowHandle, Mod::get()->getSettingValue<std::string>("winname").c_str());
-        geode::listenForSettingChanges("winname", +[](std::string value) {
-            auto GameWindowHandle = WindowFromDC(*reinterpret_cast<HDC*>(reinterpret_cast<uintptr_t>(cocos2d::CCEGLView::sharedOpenGLView()->getWindow()) + 0x244));
+
+        auto GameWindowHandle = WindowFromDC(*reinterpret_cast<HDC*>(
+            reinterpret_cast<uintptr_t>(cocos2d::CCEGLView::sharedOpenGLView()->getWindow()) + 0x244));
+        SetWindowText(GameWindowHandle, Mod::get()->getSettingValue<std::string>("windowname").c_str());
+
+        geode::listenForSettingChanges("windowname", +[](std::string value) {
+            auto GameWindowHandle = WindowFromDC(*reinterpret_cast<HDC*>(
+                reinterpret_cast<uintptr_t>(cocos2d::CCEGLView::sharedOpenGLView()->getWindow()) + 0x244));
             SetWindowText(GameWindowHandle, value.data());
-            });
+        });
+
         return true;
     }
 };
 
 void CopyFromLocal() {
-    auto game_dir = Mod::get()->getConfigDir() / "settings.json"; //from_game_dir
-    auto save_dir = Mod::get()->getSaveDir() / "settings.json"; //from_game_dir
+    auto game_dir = Mod::get()->getConfigDir() / "settings.json";
+    auto save_dir = Mod::get()->getSaveDir() / "settings.json";
+
     if (!ghc::filesystem::exists(game_dir)) return;
-    //if (!ghc::filesystem::exists(save_dir)) return;
+
     ghc::filesystem::copy(
-        game_dir, //from game dir
-        save_dir, //to game save dir
-        ghc::filesystem::copy_options::overwrite_existing);
-};
+        game_dir,
+        save_dir,
+        ghc::filesystem::copy_options::overwrite_existing
+    );
+}
+
 void CopyFromData() {
-    auto game_dir = Mod::get()->getConfigDir() / "settings.json"; //from_game_dir
-    auto save_dir = Mod::get()->getSaveDir() / "settings.json"; //from_game_dir
-    //if (!ghc::filesystem::exists(game_dir)) return;
+    auto game_dir = Mod::get()->getConfigDir() / "settings.json";
+    auto save_dir = Mod::get()->getSaveDir() / "settings.json";
+
     if (!ghc::filesystem::exists(save_dir)) return;
+
     ghc::filesystem::copy(
-        save_dir, //to game save dir
-        game_dir, //from game dir
-        ghc::filesystem::copy_options::overwrite_existing);
-};
+        save_dir,
+        game_dir,
+        ghc::filesystem::copy_options::overwrite_existing
+    );
+}
+
 $on_mod(Loaded) {
     CopyFromLocal();
     Mod::get()->loadData();
-};
-#include <Geode/modify/AppDelegate.hpp>
+}
+
 class $modify(AppDelegate) {
     void trySaveGame(bool p0) {
         AppDelegate::trySaveGame(p0);
